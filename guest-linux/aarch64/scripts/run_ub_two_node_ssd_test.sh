04a0bf14748e2935068347091f3ed2d9e5db600c
Liang Li <liliang6@me.com>
2026-06-18 10:21:52 +0800

    Run ssd test via run_app

--- guest-linux/aarch64/scripts/run_ub_two_node_ssd_test.sh
+++ guest-linux/aarch64/scripts/run_ub_two_node_ssd_test.sh
@@ -86,7 +86,7 @@ start_node() {
       "${qemu_extra[@]}" \
       -kernel "$KERNEL_IMAGE" \
       -initrd "$INITRAMFS_IMAGE" \
-      -append "console=ttyAMA0 rdinit=/bin/run_demo linqu_ssd_test=1 linqu_urma_dp_role=${role} linqu_node_idx=${node_idx} ${APPEND_EXTRA}" \
+      -append "console=ttyAMA0 rdinit=/bin/run_app linqu_ssd_test=1 linqu_urma_dp_role=${role} linqu_node_idx=${node_idx} ${APPEND_EXTRA}" \
       >"$qemu_log" 2>&1 &
   echo $! > "$pid_file"
 }
